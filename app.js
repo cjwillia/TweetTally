0f9c64384a5d8b6e325f9590e5b4b7fe3cec49a7
Cory Williams <cjwillia@andrew.cmu.edu>
2015-02-19 15:43:04 -0500

    changes made to initialization to hide application credentials

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var router = express.Router();
 
 app.set('port', (process.env.PORT || 5000));
 
+
 var static_options = {
 
 }
 
 app.use(express.static('public', static_options));
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_key: process.env.TWITTER_ACCESS_KEY,
+	access_secret: process.env.TWITTER_ACCESS_SECRET
+});
 
 app.listen(app.get('port'), function() {
 	console.log("Node app is running locally on port: " + app.get('port'));
