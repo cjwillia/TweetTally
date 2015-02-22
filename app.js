f3db57c59aba2996afc4e7dac4d65f79a749cac2
Cory Williams <cjwillia@andrew.cmu.edu>
2015-02-22 09:42:09 -0500

    remove error message from node-twitter response

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/login', function(req, res) {
 	// var handle = req.params.handle;
 	// handle === "" ? client.authorize(respond) : client.authorize_user(handle, respond);
 	client.get('oauth/authorize', {}, function(error, response){
-		if(error) throw error;
+		//if(error) throw error;
 		console.log(response);
 		res.send("<p>Something Happened!11!</p>")
 	});
