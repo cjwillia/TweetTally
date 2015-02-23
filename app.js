394348280551e2c5beaa2e469251f4801edd1788
Cory Williams <cjwillia@andrew.cmu.edu>
2015-02-23 16:56:30 -0500

    addition of mongodb and mongoose

--- app.js
+++ app.js
@@ -8,24 +8,30 @@ app.use(session({
 	keys: ['thisiskey1', 'thisiskey2']
 }));
 
-var static_options = {
-
-}
-
-app.use(express.static('public', static_options));
+app.use(express.static('public', {}));
 
 var Twitter = require('twitter');
 
-var client= new Twitter({
+var client = new Twitter({
 	consumer_key: process.env.TWITTER_CONSUMER_KEY,
 	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
 	access_token_key: process.env.TWITTER_ACCESS_KEY,
 	access_token_secret: process.env.TWITTER_ACCESS_SECRET
 });
 
+// is this necessary now with mongoose?
+var mongodb = require('mongodb');
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+	console.log('database opened!');
+});
+
 var params = {screen_name: 'thedreadjesus'};
-
-
 
 app.get('/reqToken', function(req, res) {
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
