dd4552716f60354123afeea95fbe7d5417aa3c59
Cory Williams <cjwillia@andrew.cmu.edu>
2015-03-21 19:20:56 -0400

    separtaing tweet gathering from chart drawing

---
+++ public/js/linechart.js
@@ -0,0 +1,35 @@
+function drawChart() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Hour');
+    data.addColumn('number', 'Tweets');
+
+    var rows = [];
+    var i = 0;
+    while(i < 24) {
+        rows.push(["0" + i.toString() + ":00", 0]);
+        i++;
+    }
+
+    tweets.forEach(function(tweet) {
+        rows[tweet.hour][1]++;
+    });
+
+    data.addRows(rows);
+
+    // Set chart options
+    var options = {'title':'Tweets per Hour',
+    'width':400,
+    'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
+
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
--- public/js/tweets.js
+++ public/js/tweets.js
@@ -12,7 +12,6 @@ function getCookie(cname) {
     return "";
 }
 
-
 function getDBTweets(cb) {
 	$.getJSON('/tweets/' + getCookie('user'), cb);
 }
@@ -24,53 +23,8 @@ function loadTweets(data) {
         tweet.date = new Date(tweet.date);
         tweet.hour = tweet.date.getHours();
     });
-    t.dateGraphData = function() {
-
-    }
     tweets = t;
 
-    // Load the Visualization API and the piechart package.
-    google.load('visualization', '1.0', {'packages':['corechart']});
-
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.setOnLoadCallback(drawChart);
 }
-
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-function drawChart() {
-
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Hour');
-    data.addColumn('number', 'Tweets');
-
-    var rows = [];
-    var i = 0;
-    while(i < 24) {
-        rows.push(["0" + i.toString() + ":00", 0]);
-        i++;
-    }
-
-    tweets.forEach(function(tweet) {
-        rows[tweet.hour][1]++;
-    });
-
-    console.log(rows);
-
-    data.addRows(rows);
-
-    // Set chart options
-    var options = {'title':'Tweets per Hour',
-    'width':400,
-    'height':300};
-
-    // Instantiate and draw our chart, passing in some options.
-    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-    chart.draw(data, options);
-}
-
 
 getDBTweets(loadTweets);
