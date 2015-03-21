df7b36645d3478bb55f2613336b0afe3017a40d7
Cory Williams <cjwillia@andrew.cmu.edu>
2015-03-21 17:57:47 -0400

    bugfix in object dependencies

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -28,19 +28,14 @@ function loadTweets(data) {
 
     }
     tweets = t;
+
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
 }
 
-function graphTweets() {
-
-}
-
-getDBTweets(loadTweets);
-
-// Load the Visualization API and the piechart package.
-google.load('visualization', '1.0', {'packages':['corechart']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-google.setOnLoadCallback(drawChart);
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -74,3 +69,6 @@ function drawChart() {
     var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
     chart.draw(data, options);
 }
+
+
+getDBTweets(loadTweets);
