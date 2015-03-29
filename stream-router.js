8640a9924976a49237048f232eac9762687fac46
Cory Williams <cjwillia@andrew.cmu.edu>
2015-03-29 13:49:16 -0400

    typo in stream router

--- stream-router.js
+++ stream-router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(models, client) {
-	var Stream = models.stream;
+	var Stream = models.Stream;
 
 	router.post('/', function(req, res) {
 
